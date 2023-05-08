6f947fca16e86a52dd42129c7c783147523eee01
Krzysztof Garbowicz <kgar@netcompany.com>
2023-05-08 13:35:27 +0200

    update requirements

--- Test_prediction.py
+++ Test_prediction.py
@@ -2,13 +2,10 @@
 import joblib
 import pickle
 
-def prepare(review):
-    cvFile='c1_BoW_Sentiment_Model.pkl'
-    cv = pickle.load(open(cvFile, "rb"))
-    processed_input = cv.transform([review]).toarray()[0]
-    return processed_input
-
-classifier = joblib.load('c2_Classifier_Sentiment_Model.joblib')
-prediction = classifier.predict([prepare("")])[0]
+classifier = joblib.load('output/c2_Classifier_Sentiment_Model.joblib')
+print(prepare("Wow... Loved this place."))
+prediction = classifier.predict([
+    prepare("Wow... Loved this place.")
+    ])[0]
 print(prediction)
 # 0 is negative and 1 is positive
