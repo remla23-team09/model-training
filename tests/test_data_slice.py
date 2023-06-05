4d37819c2179f0fc7317b37148eb8e7d032462b3
rieschel <emilia.rieschel@live.com>
2023-06-05 11:52:45 +0200

    Cleaned up tests

--- tests/test_data_slice.py
+++ tests/test_data_slice.py
@@ -1,33 +1,32 @@
-from src.models.train_model import evaluate_model, train_model_loaded_data
-from src.data.make_dataset import _preprocess
 import joblib
 import pytest
 import pandas
+from src.models.train_model import evaluate_model, train_model_loaded_data
+from src.data.make_dataset import preprocess_test
 
 @pytest.fixture()
 def trained_model():
-    trained_model = joblib.load("./models/sentiment_model.joblib")
-    yield trained_model
+    model = joblib.load("./models/sentiment_model.joblib")
+    yield model
 
 @pytest.fixture()
 def test_data():
-    test_data = pandas.read_csv("./data/raw/a1_RestaurantReviews_HistoricDump.tsv", sep='\t', quoting=3)
-    yield test_data
+    test_file = "./data/raw/a1_RestaurantReviews_HistoricDump.tsv"
+    data = pandas.read_csv(test_file, sep='\t', quoting=3)
+    yield data
+
+def data_slice(trained_model, sliced_data):
+    processed_data_filepath = "./data/raw/a1_RestaurantReviews_HistoricDump.tsv"
+    raw_data_filepath = "./data/processed/processed_data.joblib"
+    original_score = evaluate_model(trained_model, processed_data_filepath, raw_data_filepath)
+    preprocessed_data = preprocess_test(sliced_data)
+    sliced_score = train_model_loaded_data(trained_model, sliced_data, preprocessed_data)
+    return abs(original_score - sliced_score)
 
 def test_data_slice_negative(trained_model, test_data):
-    processed_data_filepath = "./data/raw/a1_RestaurantReviews_HistoricDump.tsv"
-    raw_data_filepath = "./data/processed/processed_data.joblib"
-    original_score = evaluate_model(trained_model, processed_data_filepath, raw_data_filepath)
-    sliced_data = test_data[test_data['Liked'] == 0]
-    _preprocess(sliced_data.reset_index(), "test.joblib")
-    sliced_score = train_model_loaded_data(trained_model, sliced_data.reset_index(), "test.joblib")
-    assert abs(original_score - sliced_score) <= 0.3
+    sliced_data = test_data[test_data['Liked'] == 0].reset_index()
+    assert data_slice(trained_model, sliced_data) <= 0.2
 
 def test_data_slice_positive(trained_model, test_data):
-    processed_data_filepath = "./data/raw/a1_RestaurantReviews_HistoricDump.tsv"
-    raw_data_filepath = "./data/processed/processed_data.joblib"
-    original_score = evaluate_model(trained_model, processed_data_filepath, raw_data_filepath)
-    sliced_data = test_data[test_data['Liked'] == 1]
-    _preprocess(sliced_data.reset_index(), "test.joblib")
-    sliced_score = train_model_loaded_data(trained_model, sliced_data.reset_index(), "test.joblib")
-    assert abs(original_score - sliced_score) <= 0.3
+    sliced_data = test_data[test_data['Liked'] == 1].reset_index()
+    assert data_slice(trained_model, sliced_data) <= 0.2
