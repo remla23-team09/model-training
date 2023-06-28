4a0843d61773fbe31821f81f8b3bc6990b6beb25
Zhiyong <200~z13913982280@gmail.com>
2023-06-28 23:55:56 +0200

    tests added,updated README.md, edit doc

--- tests/data_feature_test.py
+++ tests/data_feature_test.py
@@ -16,5 +16,3 @@ def test_distribution(test_data):
     positive = len(test_data[test_data["Liked"] == 1].reset_index())
     negative = len(test_data[test_data["Liked"] == 0].reset_index())
     assert abs(1 - positive / negative) <= 0.3
-
-
